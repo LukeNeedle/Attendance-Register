410e4c39104d470ccc0bc531a308c30f489938a1
LukeNeedle <112342734+LukeNeedle@users.noreply.github.com>
2024-03-22 17:36:39 +0000

    Fixed desktop_shortcut
    Added lots of error handling

--- desktop_shortcut.py
+++ desktop_shortcut.py
@@ -1,49 +1,64 @@
-import win32com.client
-import json
-import ctypes
-import sys
-import os
+try:
+    import win32com.client
+    import json
+    import ctypes
+    import sys
+    import os
 
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+    def is_admin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
 
-def create_shortcut(shortcutPath, targetPath, iconPath=None, workingDirectory=None):
-    shell = win32com.client.Dispatch("WScript.Shell")
-    shortcut = shell.CreateShortCut(shortcutPath)
-    shortcut.Targetpath = targetPath
-    if iconPath:
-        shortcut.IconLocation = iconPath
-    if workingDirectory:
-        shortcut.WorkingDirectory = workingDirectory
-    
-    shortcut.save()
-
-def modify_shortcut(shortcutPath, targetPath=None, iconPath=None, workingDirectory=None):
-    shell = win32com.client.Dispatch("WScript.Shell")
-    shortcut = shell.CreateShortCut(shortcutPath)
-    if targetPath:
+    def create_shortcut(shortcutPath, targetPath, iconPath=None, workingDirectory=None):
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shortcut = shell.CreateShortCut(shortcutPath)
         shortcut.Targetpath = targetPath
-    if workingDirectory:
-        shortcut.WorkingDirectory = workingDirectory
-    if iconPath:
-        shortcut.IconLocation = iconPath
-    shortcut.save()
+        if iconPath:
+            shortcut.IconLocation = iconPath
+        if workingDirectory:
+            shortcut.WorkingDirectory = workingDirectory
+        
+        shortcut.save()
 
-if not is_admin():
-    print("For the desktop shortcut to update, please allow \"desktop_shortcut.exe\" app to make changes to your device")
-    input("The popup will appear when you press enter")
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-    exit()
+    def modify_shortcut(shortcutPath, targetPath=None, iconPath=None, workingDirectory=None):
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shortcut = shell.CreateShortCut(shortcutPath)
+        if targetPath:
+            shortcut.Targetpath = targetPath
+        if workingDirectory:
+            shortcut.WorkingDirectory = workingDirectory
+        if iconPath:
+            shortcut.IconLocation = iconPath
+        shortcut.save()
 
-with open("Temp/shortcut_info.json", "r") as f:
-    shorcutInfo = json.loads(f.read())
+    if not is_admin():
+        print("For the desktop shortcut to update, please allow \"desktop_shortcut.exe\" app to make changes to your device")
+        input("The popup will appear when you press enter")
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        exit()
 
-if shorcutInfo['type'] == "modify":
-    modify_shortcut(shorcutInfo['shortcutPath'], shorcutInfo['targetPath'], shorcutInfo['iconPath'], shorcutInfo['workingDirectory'])
-else:
-    create_shortcut(shorcutInfo['shortcutPath'], shorcutInfo['targetPath'], shorcutInfo['iconPath'], shorcutInfo['workingDirectory'])
+    tempDir = (os.environ.get('TMPDIR') or os.environ.get('TEMP') or os.environ.get('TMP'))
+    if tempDir:
+        tempDir += "/Attendance_Register/"
+        if os.path.exists(tempDir + "shortcut_info.json"):
+            with open((tempDir + "shortcut_info.json"), "r") as f:
+                shorcutInfo = json.loads(f.read())
 
-os.remove("Temp/shortcut_info.json")
+            if shorcutInfo['type'] == "modify":
+                modify_shortcut(shorcutInfo['shortcutPath'], shorcutInfo['targetPath'], shorcutInfo['iconPath'], shorcutInfo['workingDirectory'])
+            else:
+                create_shortcut(shorcutInfo['shortcutPath'], shorcutInfo['targetPath'], shorcutInfo['iconPath'], shorcutInfo['workingDirectory'])
+
+            os.remove(tempDir + "shortcut_info.json")
+        else:
+            print("An error occured, please contact the distributor of this program")
+    else:
+        print("An error occured, please contact the distributor of this program")
+
+    input("Press enter to exit...")
+except Exception as error:
+    print(f"An exception occurred: {error}")
+    print("An error occured, please contact the distributor of this program")
+    input("Press enter to exit...")
